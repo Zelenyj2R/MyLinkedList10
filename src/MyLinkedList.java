5a44f327cd12d8950151b729bc54e63c5d5da86a
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-04 18:56:28 +0100

    Update MyLinkedList.java

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,10 +1,9 @@
 import java.util.Objects;
 
-public class MyLinkedList <T> implements List11 <T> {
-    static class Node <T> {
+public class MyLinkedList<T> implements List11<T> {
+    static class Node<T> {
         T element;
         Node<T> next;
-
         public Node(T element) {
             this.element = element;
         }
@@ -12,28 +11,24 @@ public class MyLinkedList <T> implements List11 <T> {
     private Node<T> first;
     private Node<T> last;
     private int size;
-    private Node<T> getNodebyIndex(int index){
+    private Node<T> getNodebyIndex(int index) {
         Node<T> current = first;
         for (int i = 0; i < index; i++) {
             current = current.next;
-            
         }
         return current;
-
     }
     @Override
     public void add(T element) {
-        Node <T> newNode = new Node<T>(element);
-        if (size == 0){
+        Node<T> newNode = new Node<T>(element);
+        if (size == 0) {
             first = last = newNode;
         } else {
             last.next = newNode;
             last = newNode;
         }
         size++;
-
     }
-
     @Override
     public T get(int index) {
         Objects.checkIndex(index, size);
@@ -43,17 +38,17 @@ public class MyLinkedList <T> implements List11 <T> {
     public T remove(int index) {
         Objects.checkIndex(index, size);
         T removedElement;
-        if(index==0){
+        if (index == 0) {
             removedElement = first.element;
             first = first.next;
-            if (first == null){
+            if (first == null) {
                 last = null;
             }
         } else {
             Node<T> prev = getNodebyIndex(index - 1);
             removedElement = prev.next.element;
             prev.next = prev.next.next;
-            if(index == size - 1){
+            if (index == size - 1) {
                 last = prev;
             }
         }
